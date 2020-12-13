6ccba682233f62074f2550e611a643e2f90089ba
Curly Bryce <curlybryce@protonmail.com>
2020-12-13 01:37:46 -0700

    updated background.sh

--- background.sh
+++ background.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
-imgdir="$HOME/media/pictures/Desktop/"
-images=$(find ${imgdir})
-feh --randomize --bg-fill ${images};
-exit
+#!/bin/sh
+# Background script 0.5 by Curly
+# Requires shuf, pywal
+store="/etc/lightdm/bg/image"
+imgdir="${HOME}/media/pictures/desktop/"
+
+if [[ $1 == "rand" ]]; then
+  img=$(find ${imgdir} -type f -printf "%h/%f\n" | shuf -n1)
+elif [[ $1 == "last" ]]; then
+  img=${store}
+fi
+
+cp "${img}" /etc/lightdm/bg/image;
+wal -i "${img}" --saturate 0.8;
